d27824f8d5d9dd0b9c4c35e42376eaa6923484ad
Ryan Miyahara <rmiyahara144@gmail.com>
2018-11-20 14:06:28 -0800

    Prepped mounting

--- lab3a.c
+++ lab3a.c
@@ -8,15 +8,23 @@ ID: 104844623,804585999
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include "ext2_fs.h"
+#include <unistd.h>
 
 //Global Variables
+int mount_fd;
 
 int main(int argc, char** argv) {
     if (argc != 2) {
         fprintf(stderr, "Incorrect usage of the program. Please use the program in the following format: ./lab3a FILE_SYSTEM_IMAGE\n");
         exit(1);
     }
-    if (debug) debug_print(0);
+    
+    mount_fd = read(argv[1], O_RDONLY);
+    if (mount_fd < 0) {
+        fprintf(stderr, "Unable to mount file system.\nError message: %s\nError number: %d\n", strerror(errno), errno);
+        exit(2);
+    }
 
     exit(0); //Successful exit
 }
