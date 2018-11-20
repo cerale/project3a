49a2f8df66c7533be027f37b51fc0fd4e0c3500d
Ryan Miyahara <rmiyahara144@gmail.com>
2018-11-19 21:35:48 -0800

    No I wrote the whole program

--- lab3a.c
+++ lab3a.c
@@ -10,25 +10,12 @@ ID: 104844623,804585999
 #include <string.h>
 
 //Global Variables
-bool debug = false;
-
-void debug_print(int mes) {
-    switch(mes) {
-        case 0:
-            printf("Debug mode activated!\n");
-            break;
-        default:
-            printf("You shouldn't get here!\n");
-    }
-    return;
-}
 
 int main(int argc, char** argv) {
     if (argc != 2) {
         fprintf(stderr, "Incorrect usage of the program. Please use the program in the following format: ./lab3a FILE_SYSTEM_IMAGE\n");
         exit(1);
     }
-    printf("%s\n", argv[1]);
     if (debug) debug_print(0);
 
     exit(0); //Successful exit
