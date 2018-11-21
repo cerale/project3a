20848df53426915cdce5b300bec72a053d854d80
Ryan Miyahara <rmiyahara144@gmail.com>
2018-11-20 19:51:34 -0800

    Added group and free blocks

--- lab3a.c
+++ lab3a.c
@@ -19,20 +19,49 @@ ID: 104844623,804585999
 int mount_fd;
 int superblock_offset = 1024;
 struct ext2_super_block thuper;
+__uint32_t block_count;
+__uint32_t inode_count;
 __uint32_t block_size;
 __uint32_t inode_size;
+struct ext2_group_desc groupie;
 struct ext2_inode inode;
-struct ext2_group_desc group;
 struct ext2_dir_entry dir;
 
-void print_superblock_summary() {
-    if(pread(mount_fd, &thuper, sizeof(struct ext2_super_block), superblock_offset) < 0){
-        fprintf(stderr, "Unable to pread.\nError message: %s\nError number: %d\n", strerror(errno), errno);
+void superblock_summary() {
+    if (pread(mount_fd, &thuper, sizeof(struct ext2_super_block), superblock_offset) < 0){
+        fprintf(stderr, "Unable to pread for superblock.\nError message: %s\nError number: %d\n", strerror(errno), errno);
         exit(2);
     }
+    block_count = thuper.s_blocks_count;
+    inode_count = thuper.s_inodes_count;
     block_size = EXT2_MIN_BLOCK_SIZE << thuper.s_log_block_size;
     inode_size = thuper.s_inode_size;
-    fprintf(stdout, "SUPERBLOCK,%u,%u,%u,%u,%u,%u,%u\n", thuper.s_blocks_count, thuper.s_inodes_count, block_size, inode_size, thuper.s_blocks_per_group, thuper.s_inodes_per_group, thuper.s_first_ino);
+    fprintf(stdout, "SUPERBLOCK,%u,%u,%u,%u,%u,%u,%u\n", block_count, inode_count, block_size, inode_size, thuper.s_blocks_per_group, thuper.s_inodes_per_group, thuper.s_first_ino);
+    return;
+}
+
+void group_summary() {
+    if (pread(mount_fd, &groupie, sizeof(struct ext2_group_desc), superblock_offset + sizeof(struct ext2_super_block)) < 0) {
+        fprintf(stderr, "Unable to pread for group.\nError message: %s\nError number: %d\n", strerror(errno), errno);
+        exit(2);
+    }
+    fprintf(stdout, "GROUP,0,%u,%u,%u,%u,%u,%u,%u\n", block_count, inode_count, groupie.bg_free_blocks_count, groupie.bg_free_inodes_count, groupie.bg_block_bitmap, groupie.bg_inode_bitmap, groupie.bg_inode_table);
+    return;
+}
+
+void free_blocks() {
+    __uint32_t block_bitmap = groupie.bg_block_bitmap;
+    for (__uint32_t i = 0; i < block_count; i++) {
+        unsigned char offset = (i >> 3) & 255;
+        unsigned char buffer;
+        if (pread(mount_fd, &buffer, sizeof(unsigned char), block_bitmap * block_size + offset) < 0) {
+            fprintf(stderr, "Unable to pread for free blocks.\nError message: %s\nError number: %d\n", strerror(errno), errno);
+            exit(2);
+        }
+        bool is_free = ((buffer >> (i & 7)) & 1);
+        if (!(is_free))
+            fprintf(stdout, "BREE,%d\n", i + 1);
+    }
     return;
 }
 
@@ -48,6 +77,9 @@ int main(int argc, char** argv) {
         exit(2);
     }
 
-    print_superblock_summary();
+    superblock_summary();
+    group_summary();
+    free_blocks();
+
     exit(0); //Successful exit
 }
