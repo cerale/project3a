4c5e05721015bc7f30dee2d88b7e5a7c37281e34
Ryan Miyahara <rmiyahara144@gmail.com>
2018-11-20 19:06:52 -0800

    Completed super block analysis

--- lab3a.c
+++ lab3a.c
@@ -10,9 +10,31 @@ ID: 104844623,804585999
 #include <string.h>
 #include "ext2_fs.h"
 #include <unistd.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 //Global Variables
 int mount_fd;
+int superblock_offset = 1024;
+struct ext2_super_block thuper;
+__uint32_t block_size;
+__uint32_t inode_size;
+struct ext2_inode inode;
+struct ext2_group_desc group;
+struct ext2_dir_entry dir;
+
+void print_superblock_summary() {
+    if(pread(mount_fd, &thuper, sizeof(struct ext2_super_block), superblock_offset) < 0){
+        fprintf(stderr, "Unable to pread.\nError message: %s\nError number: %d\n", strerror(errno), errno);
+        exit(2);
+    }
+    block_size = EXT2_MIN_BLOCK_SIZE << thuper.s_log_block_size;
+    inode_size = thuper.s_inode_size;
+    fprintf(stdout, "SUPERBLOCK,%u,%u,%u,%u,%u,%u,%u\n", thuper.s_blocks_count, thuper.s_inodes_count, block_size, inode_size, thuper.s_blocks_per_group, thuper.s_inodes_per_group, thuper.s_first_ino);
+    return;
+}
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -20,11 +42,12 @@ int main(int argc, char** argv) {
         exit(1);
     }
     
-    mount_fd = read(argv[1], O_RDONLY);
+    mount_fd = open(argv[1], O_RDONLY);
     if (mount_fd < 0) {
         fprintf(stderr, "Unable to mount file system.\nError message: %s\nError number: %d\n", strerror(errno), errno);
         exit(2);
     }
 
+    print_superblock_summary();
     exit(0); //Successful exit
 }
